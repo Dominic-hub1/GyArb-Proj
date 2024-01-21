d1f76adca8166f56a3b9123300afb34030ff6ee1
Dominic-hub1 <124262331+Dominic-hub1@users.noreply.github.com>
2024-01-21 22:27:00 +0100

    Movent klar

--- GyArb Proj/Assets/PlayMovement.cs
+++ GyArb Proj/Assets/PlayMovement.cs
@@ -5,10 +5,25 @@ public class PlayMovement : MonoBehaviour
     // this is a refrence to rigidbody called rb
     public Rigidbody rb;
 
+    public float forwaedForce = 2000f;
+    public float sidawaysForce = 500f;
+
+
     // We marked this as "Fixed"Update because we
     // are is to mess with physic.
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime); //Add a force to z axis 2000
+        rb.AddForce(0, 0, forwaedForce * Time.deltaTime); //Add a forward force 
+
+        if( Input.GetKey("d"))
+        {
+            rb.AddForce(sidawaysForce * Time.deltaTime, 0, 0);
+        }
+
+         if( Input.GetKey("a"))
+        {
+            rb.AddForce(-sidawaysForce * Time.deltaTime, 0, 0);
+        }
+    
     } 
 }
