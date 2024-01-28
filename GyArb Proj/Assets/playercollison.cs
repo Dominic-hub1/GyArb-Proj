473bc67436fec05e3feeb9d4a4de6f0691e24b83
Manhub3 <155843298+Manhub3@users.noreply.github.com>
2024-01-28 17:22:42 +0100

    d

---
+++ GyArb Proj/Assets/playercollison.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+public class playercollison : MonoBehaviour {
+
+    public PlayMovement movement;
+
+    void OnCollisionEnter (Collision collisionInfo) 
+    {
+        if (collisionInfo.collider.tag == "obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+
+
+
+
+}
